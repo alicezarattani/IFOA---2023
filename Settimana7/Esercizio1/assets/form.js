6cb8c13a89fcc1ab33b4ec12014970d01a3bf597
Alice Zarattani <alice.zarattani@gmail.com>
2023-12-05 17:32:16 +0100

    Edit

--- Settimana7/Esercizio1/assets/form.js
+++ Settimana7/Esercizio1/assets/form.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form");
-const taskContainer = document.querySelector("#task");
+const taskContainer = document.querySelector("#task"); //la useremo alla fine per visualizzare la lista dei pet
 const petList = [];
 
 class Pet {
@@ -11,27 +11,36 @@ class Pet {
   }
 }
 
-//raccogli i dati dal form e crea diverse istanze della classe Pet mostrandone i dati in una lista
-
+//aggiungiamo un eventListener al form che si attiva quando viene inviato con submit
 form.addEventListener("submit", (e) => {
+  //il metodo e.preventDefault() previene il comportamento predefinito del form (come il ricaricamento della pagina).
   e.preventDefault();
+  //recuperiamo i valori dei campi del form
   const petName = document.querySelector("input[name=petName]").value;
   const ownerName = document.querySelector("input[name=ownerName]").value;
   const species = document.querySelector("input[name=species]").value;
   const breed = document.querySelector("input[name=breed]").value;
 
+  //creiamo una nuova istanza della classe Pet (un nuovo oggetto) con i valori di Pet (il nostro stampino)
   const newPet = new Pet(petName, ownerName, species, breed);
+
+  //pushiamo i valori della nuova istanza della classe Pet nella petList
   petList.push(newPet);
-  console.log();
+
+  //chiamiamo la funzione che stampa la lista dei pet
   displayPetList();
 });
 
 function displayPetList() {
-  taskContainer.innerHTML = "";
+  taskContainer.innerHTML = ""; //rimuoviamo gli eventuali elementi preesistenti all'interno dell'elemento con id "task" (a cui fa riferimento taskContainer)
 
+  //iteriamo l'array petList con un forEach per ogni istanza di Pet
   petList.forEach((pet) => {
+    //per ogni pet viene creato un nuovo elemento div in cui vengono stampati i dettagli del pet
     const petItem = document.createElement("div");
     petItem.textContent = `Pet: ${pet.petName}, Owner: ${pet.ownerName}, Species: ${pet.species}, Breed: ${pet.breed}`;
+
+    //l'elemento div viene aggiunto come elemento figlio all'elemento taskContainer -> gli elementi della petList verranno visualizzati nella taskContainer (i petItem)
     taskContainer.appendChild(petItem);
   });
 }

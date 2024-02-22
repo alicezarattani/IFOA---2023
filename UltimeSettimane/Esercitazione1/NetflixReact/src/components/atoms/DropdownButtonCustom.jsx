4838c438e1053063909a33fc0e7a41a4c4138767
Alice Zarattani <alice.zarattani@gmail.com>
2024-02-22 15:08:21 +0100

    modified

--- UltimeSettimane/Esercitazione1/NetflixReact/src/components/atoms/DropdownButtonCustom.jsx
+++ UltimeSettimane/Esercitazione1/NetflixReact/src/components/atoms/DropdownButtonCustom.jsx
@@ -13,9 +13,9 @@ function DropdownButtonCustom() {
       </Dropdown.Toggle>
 
       <Dropdown.Menu className="dropdown-menu-end">
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">Manage Profiles</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Account</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Help Center</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
--- UltimeSettimane/Esercitazione1/NetflixReact/src/components/atoms/DropdownGeneri.jsx
+++ UltimeSettimane/Esercitazione1/NetflixReact/src/components/atoms/DropdownGeneri.jsx
@@ -13,8 +13,8 @@ function DropdownGeneri() {
 
       <Dropdown.Menu className="dropdown-menu-end">
         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Comedy</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Horror</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
